beeb5cc3f8fcdc39b07ebbc61049208842773668
Shivam Sharma <16ucc088@lnmiit.ac.in>
2020-05-05 18:57:31 +0530

    Update RansomNote.py

--- Day-3/RansomNote.py
+++ Day-3/RansomNote.py
@@ -1,3 +1,10 @@
+#Given an arbitrary ransom note string and another string containing letters from all the magazines,
+#write a function that will return true if the ransom note can be constructed from the magazines ; 
+#otherwise, it will return false.
+#Each letter in the magazine string can only be used once in your ransom note.
+
+#Solution:
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         r= list(ransomNote)
